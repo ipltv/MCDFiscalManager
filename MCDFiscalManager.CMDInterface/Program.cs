23ab0d39834fa932531ce423764ac7349c507d26
Илья Платович <platovich@yandex.ru>
2019-10-06 12:40:42 +0300

    Добавьте файлы проекта.

---
+++ MCDFiscalManager.CMDInterface/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCDFiscalManager.CMDInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
