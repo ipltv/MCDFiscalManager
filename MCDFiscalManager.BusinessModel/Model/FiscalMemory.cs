4fcae735d0b030810cf03eca218044ba4438339b
Илья Платович <platovich@yandex.ru>
2019-10-06 15:21:15 +0300

    Реализация классов представляющие фискальный принтер и фискальный накопитель.

---
+++ MCDFiscalManager.BusinessModel/Model/FiscalMemory.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCDFiscalManager.BusinessModel.Model
+{
+    /// <summary>
+    /// Класс представляющий фискальную память принтера - фискальный накопитель (ФН).
+    /// </summary>
+    public class FiscalMemory
+    {
+        #region Properties
+        /// <summary>
+        /// Серийный номер фискального накопителя. Только для чтения.
+        /// </summary>
+        public string SerialNumber { get; }
+        /// <summary>
+        /// Модель фискального накопителя. Только для чтения.
+        /// </summary>
+        public string Model { get; }
+        /// <summary>
+        /// Дата регистрации фискального накопителя в НО.Только для чтения.
+        /// </summary>
+        public DateTime? RegistrationDate { get; }
+        /// <summary>
+        /// Дата окончания срока действия фискального накопителя. Только для чтения.
+        /// </summary>
+        public DateTime? ExpirationDate { get; }
+        #endregion
+        #region Сonstructors
+        /// <summary>
+        /// Создает экзепляр класса представляющий фискальный накопитель (ФН).
+        /// </summary>
+        /// <param name="serialNumber">Серийный номер ФН</param>
+        /// <param name="model">Модель ФН</param>
+        /// <param name="registrationDate">Дата регистрации</param>
+        /// <param name="expirationDate">Дата окончания срока дейтсвия</param>
+        public FiscalMemory 
+            (string serialNumber, 
+            string model, 
+            DateTime? registrationDate = null, 
+            DateTime? expirationDate = null)
+        {
+            #region DataChek
+            if (string.IsNullOrWhiteSpace(serialNumber)) 
+            {
+                throw new ArgumentNullException("Серийный номер не может быть пустым или null.",nameof(serialNumber));
+            }
+            if (string.IsNullOrWhiteSpace(model))
+            {
+                throw new ArgumentNullException("Модель не может быть пустой или null.", nameof(model));
+            }
+            if (registrationDate >= expirationDate)
+            {
+                throw new ArgumentException("Дата регистрациии ФН не может быть позже или равной дате окончания срока действия ФН.", nameof(registrationDate));
+            }
+            #endregion
+            SerialNumber = serialNumber;
+            Model = model;
+            RegistrationDate = registrationDate;
+            ExpirationDate = expirationDate;
+        }
+        #endregion
+    }
+}
---
+++ MCDFiscalManager.BusinessModel/Model/FiscalPrinter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCDFiscalManager.BusinessModel.Model
+{
+    /// <summary>
+    /// Класс представляющий фискальный принтер/регистратор.
+    /// </summary>
+    public class FiscalPrinter
+    {
+        #region Fields
+        /// <summary>
+        /// Поле представляющее место установки фискального принтера
+        /// </summary>
+        private string placeOfInstallation;
+        private FiscalMemory currentFiscalMemory;
+        #endregion
+        #region Properties
+        /// <summary>
+        /// Серийный номер фискального принтера. Только для чтения.
+        /// </summary>
+        public string SerialNumber
+        {
+            get;
+        }
+        /// <summary>
+        /// Модель фискального принтера. Только для чтения.
+        /// </summary>
+        public string Model { get; }
+        /// <summary>
+        /// Дата регистрации фискального принтера в НО. Только для чтения.
+        /// </summary>
+        public DateTime? RegistrationDate { get; private set; }
+        /// <summary>
+        /// Место установки фискального принтера.
+        /// </summary>
+        public string PlaceOfInstallation
+        {
+            get => placeOfInstallation;
+            set
+            {
+                placeOfInstallation = value.Trim();
+            }
+        }
+        /// <summary>
+        /// Регистрационный номер присвоенный фискальному регистратору (ФР) налоговым органом.
+        /// </summary>
+        public string RegistrationNumder { get; set; }
+        #endregion
+        #region Constructors
+        /// <summary>
+        /// Создает экземпляр каласса представляющий фискальный принтер (ФР).
+        /// </summary>
+        /// <param name="serialNumber">Серийный номер ФР.</param>
+        /// <param name="model">Модель ФР.</param>
+        /// <param name="registrationDate">Дата регистрации ФР в налоговом органе.</param>
+        /// <param name="placeOfInstallation">Место установки ФР.</param>
+        /// <param name="registrationNumber">Регистрационный номер ФР присвоенный налоговым органом.</param>
+        /// <param name="fiscalMemory">Экземпляр класса представляющий фискальный накопитель (ФН) установленный в данном ФР.</param>
+        public FiscalPrinter
+            (string serialNumber, 
+            string model, 
+            DateTime? registrationDate = null, 
+            string placeOfInstallation = "", 
+            string registrationNumber = "", 
+            FiscalMemory fiscalMemory = null)
+        {
+            if (string.IsNullOrWhiteSpace(serialNumber))
+            {
+                throw new ArgumentNullException("Серийный номер фискального принтера не может быть пустым или null.", nameof(serialNumber));
+            }
+            if (string.IsNullOrWhiteSpace(model))
+            {
+                throw new ArgumentNullException("Модель фискального принтера не может быть пустой или null.", nameof(model));
+            }
+
+            PlaceOfInstallation = placeOfInstallation ?? throw new ArgumentNullException("Место установки фискального принтера не может быть null.", nameof(placeOfInstallation));
+            RegistrationNumder = registrationNumber ?? throw new ArgumentNullException("Регистрационный номер фискального принтера не может быть null.", nameof(registrationNumber));
+            SerialNumber = serialNumber;
+            Model = model;
+            RegistrationDate = registrationDate;          
+            currentFiscalMemory = fiscalMemory;
+        }
+        #endregion
+    }
+}
