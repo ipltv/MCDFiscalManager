23ddfebd1d827a64b452d3ed9ed939735bb54a4e
Илья Платович <platovich@yandex.ru>
2019-12-15 13:53:44 +0300

    Добавлен контекст данных. Реализован класс DataBaseSaver для работы с БД.

---
+++ MCDFiscalManager.DataController/FiscalContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MCDFiscalManager.BusinessModel.Model;
+
+namespace MCDFiscalManager.DataController
+{
+    /// <summary>
+    /// Представляет контекст данный для работы с базой данных.
+    /// </summary>
+    public class FiscalContext : DbContext
+    {
+        public FiscalContext() : base("FiscalDBConnection") { }
+        
+        public DbSet<Adress> Adresses { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<FiscalMemory> FiscalMemories { get; set; }
+        public DbSet<FiscalPrinter> FiscalPrinters { get; set; }
+        public DbSet<OFD> OFDs { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
